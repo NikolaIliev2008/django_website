f6c85f59522d1cb9a0d88463539996a3354ac69b
Niki Iliev <nikola.iv.iliev@gmail.com>
2024-06-07 11:53:52 +0300

    first commit

---
+++ recipes/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+recipes = [
+    {
+      'author': 'Tom',
+      'title': 'meatball sub',
+      'directions': 'combine everything',
+      'date_posted': 'June 2024'
+    },
+    {
+      'author': 'Niki',
+      'title': 'turkey sub',
+      'directions': 'combine everything',
+      'date_posted': 'March 2024'
+    }
+]
+
+
+def home(request):
+    context = {'recipes': recipes}
+    return render(request, "recipes/home.html", context)
+
+def about(request):
+        return render(request, "recipes/about.html")
