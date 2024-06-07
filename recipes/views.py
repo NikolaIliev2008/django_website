18392bc72264f0dc0f7276b37cad0c6f316efa3d
Niki Iliev <nikola.iv.iliev@gmail.com>
2024-06-07 11:57:21 +0300

    Added navbar,card and some dummy data

--- recipes/views.py
+++ recipes/views.py
@@ -12,6 +12,12 @@ recipes = [
       'title': 'turkey sub',
       'directions': 'combine everything',
       'date_posted': 'March 2024'
+    },
+    {
+      'author': 'Tony',
+      'title': 'Chicken sub',
+      'directions': 'combine everything',
+      'date_posted': 'July 2024'
     }
 ]
 
