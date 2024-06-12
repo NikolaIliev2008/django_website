883f2033e919f384bceb9d720be37bccb3ac95b9
Niki Iliev <nikola.iv.iliev@gmail.com>
2024-06-12 11:51:01 +0300

    Made changes so that we can see the title in the admin and rewrited the html home

--- recipes/models.py
+++ recipes/models.py
@@ -6,4 +6,7 @@ class Recipe(models.Model):
     description = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     time_created = models.DateTimeField(auto_now_add=True)
-    time_updated = models.DateTimeField(auto_now_add=True)
+    time_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
