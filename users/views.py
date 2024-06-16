38988abac144574e1f1d94dce7b230d6598ac5e4
Niki Iliev <nikola.iv.iliev@gmail.com>
2024-06-16 13:17:27 +0300

    Added an new app for the users and made  login, logout, profile template. Also made a new register view

---
+++ users/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from . import forms
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LogoutView
+
+def register(request):
+    if request.method == "POST":
+        form = forms.UserRegisterForm(request.POST)
+        if form.is_valid():
+         form.save()
+         username = form.cleaned_data.get('username')
+         messages.success(request, f"{username}, your account has been created, please login!")
+         return redirect('user-login')
+    else:
+        form = forms.UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form })
+
+@login_required()
+def profile(request):
+   return render(request, 'users/profile.html')
+
+
+class CustomLogoutView(LogoutView):
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+
+
